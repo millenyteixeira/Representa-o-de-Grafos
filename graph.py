40c2acea6e712eee588c6f13b09ed8a140287b79
alexandmag <alexandmagara@gmail.com>
2023-10-16 20:13:01 -0300

    Criando função de adicionar aresta direcionada

--- graph.py
+++ graph.py
@@ -1,28 +1,47 @@
+# Matriz de adjacência do grafo
 class GraphMatrix:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
         self.graph = [[0] * num_vertices for _ in range(num_vertices)]
 
+    # Adicionar aresta simples
     def add_edge(self, v1, v2):
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             self.graph[v1][v2] = 1
             self.graph[v2][v1] = 1
 
+    # Adicionar aresta direcionada de v1 para v2
+    def add_directed_edge(self, v1, v2):
+        if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
+            self.graph[v1][v2] = 1
+            self.graph[v2][v1] = 0
+
+    # Remover aresta
     def remove_edge(self, v1, v2):
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             self.graph[v1][v2] = 0
             self.graph[v2][v1] = 0
 
+# Lista de adjacência do grafo
 class GraphAdjList:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
         self.graph = [[] for _ in range(num_vertices)]
 
+    # Adicionar aresta simples
     def add_edge(self, v1, v2):
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             self.graph[v1].append(v2)
             self.graph[v2].append(v1)
 
+    # Adicionar aresta direcionada de v1 para v2
+    def add_directed_edge(self, v1, v2):
+        if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
+            self.graph[v1].append(v2)
+            if v1 in self.graph[v2]:
+                self.graph[v2].remove(v1)
+
+    # Remover aresta
     def remove_edge(self, v1, v2):
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             if v2 in self.graph[v1]:
