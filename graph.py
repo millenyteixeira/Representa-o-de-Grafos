72fdf7b5ecef7949620ebbac75f0092858eafb24
alexandmag <alexandmagara@gmail.com>
2023-10-16 17:52:04 -0300

    Criando função de remover arestas

--- graph.py
+++ graph.py
@@ -8,6 +8,11 @@ class GraphMatrix:
             self.graph[v1][v2] = 1
             self.graph[v2][v1] = 1
 
+    def remove_edge(self, v1, v2):
+        if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
+            self.graph[v1][v2] = 0
+            self.graph[v2][v1] = 0
+
 class GraphAdjList:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
@@ -17,3 +22,10 @@ class GraphAdjList:
         if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
             self.graph[v1].append(v2)
             self.graph[v2].append(v1)
+
+    def remove_edge(self, v1, v2):
+        if 0 <= v1 < self.num_vertices and 0 <= v2 < self.num_vertices:
+            if v2 in self.graph[v1]:
+                self.graph[v1].remove(v2)
+            if v1 in self.graph[v2]:
+                self.graph[v2].remove(v1)
