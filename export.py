a018391fde76755232ab81547ac6651d8564604e
alexandmag <alexandmagara@gmail.com>
2023-10-16 23:57:44 -0300

    Correção nas funcões de exportação de grafos direcionados ou não direcionados

--- export.py
+++ export.py
@@ -1,15 +1,47 @@
-def export_directed_graph(graph, file_name):
+def export_undirected_graph(graph, file_name):
     with open(file_name, "w") as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         f.write('<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">\n')
-        f.write('  <graph defaultedgetype="mixed">\n')
+        f.write('  <graph defaultedgetype="undirected">\n')
 
-        # Escreva os nós
         f.write('    <nodes>\n')
         for i in range(len(graph)):
             f.write(f'      <node id="{i}" label="Node {i}" />\n')
         f.write('    </nodes>\n')
 
+        f.write('    <edges>\n')
+        for i in range(len(graph)):
+            for j in range(i + 1, len(graph)):
+                if graph.graph[i][j] == 1:
+                    if not graph.graph[j][i] == 0:      
+                        f.write(f'      <edge source="{i}" target="{j}"/>\n')
+        f.write('    </edges>\n')
+
+        f.write('  </graph>\n')
+        f.write('</gexf>\n')
+
+
+def export_directed_graph(graph, file_name):
+    with open(file_name, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write('<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">\n')
+        f.write('  <graph defaultedgetype="directed">\n')
+
+        f.write('    <nodes>\n')
+        for i in range(len(graph)):
+            f.write(f'      <node id="{i}" label="Node {i}" />\n')
+        f.write('    </nodes>\n')
+
+        f.write('    <edges>\n')
+        for i in range(len(graph)):
+            for j in range(len(graph)):
+                if graph.graph[i][j] == 1:
+                    f.write(f'      <edge source="{i}" target="{j}" type="directed"/>\n')
+        f.write('    </edges>\n')
+
+        f.write('  </graph>\n')
+        f.write('</gexf>\n')
+'''
         # Escreva as arestas
         f.write('    <edges>\n')
         for i in range(len(graph)):
@@ -20,9 +52,7 @@ def export_directed_graph(graph, file_name):
                     else:
                         f.write(f'      <edge source="{i}" target="{j}"/>\n')
         f.write('    </edges>\n')
-
-        f.write('  </graph>\n')
-        f.write('</gexf>\n')
+'''
 
 # Suposto código de exportação de arestas ponderadas
 '''
