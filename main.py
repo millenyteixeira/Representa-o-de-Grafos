eb6dfec78150bb09c4cf9c9e63171580a75df5d6
alexandmag <alexandmagara@gmail.com>
2023-10-17 01:46:03 -0300

    Alteração dos valores de teste

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from export import export_directed_graph, export_undirected_graph
 
 def main():
     # Crie uma instância da classe GraphMatrix e adicione arestas
-    matrix_graph = GraphMatrix(5)
-    matrix_graph.add_edge(0, 1)
-    matrix_graph.add_edge(1, 2)
-    matrix_graph.add_directed_edge(2, 3)
-    matrix_graph.add_directed_edge(3, 4)
-    matrix_graph.add_directed_edge(4, 0)
-    matrix_graph.add_directed_edge(4, 1)
-    matrix_graph.add_directed_edge(4, 2)
-    matrix_graph.add_directed_edge(4, 3)
+    matrix_graph = GraphMatrix(4)
+    # matrix_graph.add_edge(0, 1, 2)
+    # matrix_graph.add_edge(0, 2, 3)
+    # matrix_graph.add_edge(1, 2, 4)
+    # matrix_graph.add_edge(2, 3, 1)
+    matrix_graph.add_directed_edge(0, 1, 2)
+    matrix_graph.add_directed_edge(0, 2, 3)
+    matrix_graph.add_directed_edge(1, 2, 4)
+    matrix_graph.add_directed_edge(2, 3, 1)
   
 
     # Crie uma instância da classe GraphAdjList e adicione arestas
-    adj_list_graph = GraphAdjList(5)
-    adj_list_graph.add_edge(0, 1)
-    adj_list_graph.add_edge(1, 2)
-    adj_list_graph.add_edge(2, 3)
-    adj_list_graph.add_directed_edge(4, 0)
+    adj_list_graph = GraphAdjList(4)
+    # adj_list_graph.add_edge(0, 1, 2)
+    # adj_list_graph.add_edge(0, 2, 3)
+    # adj_list_graph.add_edge(1, 2, 4)
+    # adj_list_graph.add_edge(2, 3, 1)
+    adj_list_graph.add_directed_edge(0, 1, 2)
+    adj_list_graph.add_directed_edge(0, 2, 3)
+    adj_list_graph.add_directed_edge(1, 2, 4)
+    adj_list_graph.add_directed_edge(2, 3, 1)
+
 
     # Imprima as representações dos grafos
     print("Matriz de Adjacência:")
@@ -28,11 +33,14 @@ def main():
         print(row)
 
     print("Lista de Adjacência:")
+    print("exemplo: [(vertice1, peso1),(vertice2, peso2)]")
     for i, neighbors in enumerate(adj_list_graph.graph):
         print(f"Vértice {i}: {neighbors}")
 
-    export_directed_graph(matrix_graph, "Directed_graph.gexf")
-    export_undirected_graph(matrix_graph, "Undirected_graph.gexf")
+    # Funções de exportações para .gexf
+
+    #export_directed_graph(matrix_graph, "Directed_graph.gexf")
+    #export_undirected_graph(matrix_graph, "Undirected_graph.gexf")
 
 if __name__ == "__main__":
     main()
